2293fcbc609a4d1896ab4f8f37864d0e3be0e0d8
Frances Liu <thehamster@gmail.com>
2016-10-03 20:57:28 -0700

    cleaned up files

--- server.py
+++ server.py
@@ -1,25 +1,31 @@
 from jinja2 import StrictUndefined
-from flask import Flask, render_template, redirect, request
+from flask import (Flask, abort, flash, Markup, redirect,
+                   render_template, request, Response, session, url_for)
 
-app = Flask(__name__, static_url_path = "/static", static_folder = "static")
+app = Flask(__name__, static_url_path="/static", static_folder="static")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
-
-@app.route('/blog')
-def blog():
-  return render_template('blog.html')
 
 @app.route('/projects')
 def projects():
-  return render_template('projects.html')
+    return render_template('projects.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.errorhandler(404)
+def not_found(exc):
+    return Response('<h3>Not found</h3>'), 404
 
 if __name__ == '__main__':
     app.debug = True
